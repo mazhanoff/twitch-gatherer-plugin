92c2a061d799fa75a581ae874cdc827039b6a230
msmazhanov <msmazhanov@yandex-team.ru>
2022-04-04 08:56:28 +0300

    Test scripting

--- popup.js
+++ popup.js
@@ -1,6 +1,15 @@
 (function() {
     document.addEventListener("DOMContentLoaded", () => {
         console.log("init");
-        console.log(chrome);
+        const switcher = document.getElementById('switcher');
+
+        if (switcher) {
+            chrome.storage.local.get(['isActive'], res => {
+                switcher.checked = res.isActive || false;
+            })
+            switcher.addEventListener('change', (e) => {
+                chrome.storage.local.set({isActive: e.target.checked});
+            })
+        }
     })
 })()
